c982035d49eccf8cdaad6b5f1121348fb837d2dc
willyam <lyamwp@gmail.com>
2022-05-21 17:40:29 -0300

    Preparação tela sobre

--- Front-end/src/componentes/paginas/Sobre.js
+++ Front-end/src/componentes/paginas/Sobre.js
@@ -2,9 +2,35 @@ import styles from './assets/Sobre.module.css';
 
 function Sobre() {
     return(
-        <div>
-            <h1>Sobre</h1>
-        </div>
+      <div className={styles.main}>
+
+          <div className={styles.center}>
+              <div className={styles.menu}>
+                  <div className={styles.nome}>
+                      <h1>Sobre</h1>
+                      </div>
+                      <div className={styles.itemmenu}></div>
+                      <a href='#'>Saiba mais</a>
+                </div>
+                <div className={styles.form}>
+                <h2>Equipes</h2>
+                    <form>
+                        <div className={styles.buttonform}>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                        </div>
+                    </form>
+                </div>
+
+
+                      
+            </div>
+      </div> 
     )
 }
 
