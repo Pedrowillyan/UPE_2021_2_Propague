f42452d0a5f9a16d5958c5ab30cc62f555d1f56f
willyam <lyamwp@gmail.com>
2022-05-21 19:43:54 -0300

    Grid para ps campos

--- Front-end/src/componentes/paginas/Sobre.js
+++ Front-end/src/componentes/paginas/Sobre.js
@@ -1,37 +1,66 @@
 import styles from './assets/Sobre.module.css';
 
 function Sobre() {
-    return(
-      <div className={styles.main}>
-
-          <div className={styles.center}>
-              <div className={styles.menu}>
-                  <div className={styles.nome}>
-                      <h1>Sobre</h1>
-                      </div>
-                      <div className={styles.itemmenu}></div>
-                      <a href='#'>Saiba mais</a>
+    return (
+        <div> className={styles.main}
+            <div className={styles.center}>
+                <div className={styles.menu}>
+                    <div className={styles.nome}>
+                        <h1>Sobre</h1>
+                    </div>
                 </div>
-                <div className={styles.form}>
-                <h2>Equipes</h2>
-                    <form>
-                        <div className={styles.buttonform}>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                        </div>
-                    </form>
                 </div>
 
+                <div className={styles.gridContainer}>
+                    <div className={styles.gridItem}><h2>Equipes</h2></div>
+                    <div className={styles.gridItem}><h2>Mídias</h2></div>
+                    <div className={styles.gridItem}><h2>Setores</h2></div>
+                    <div className={styles.gridItem}><h2> Atividades</h2></div>
+                    <div className={styles.gridItem}><h2>Parceiros</h2></div>
+                </div>
+                <div className={styles.gridContainer}>
+                    <div className={styles.gridItem}><button>equipe xxx</button>
+                        <button>equipe xxx</button>
+                        <button>equipe xxx</button>
+                        <button>equipe xxx</button>
+                        <button>equipe xxx</button>
+                        <button>equipe xxx</button>
+                        <button>equipe xxx</button></div>
+                    <div className={styles.gridItem}><button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button></div>
+                    <div className={styles.gridItem}><button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button></div>
+                    <div className={styles.gridItem}><button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button></div>
+                    <div className={styles.gridItem}><button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button>
+                            <button>equipe xxx</button></div>
+                </div>
 
-                      
+
+
+
             </div>
-      </div> 
-    )
+            )
 }
 
-export default Sobre;
+            export default Sobre;
