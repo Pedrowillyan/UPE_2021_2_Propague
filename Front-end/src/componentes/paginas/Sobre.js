66d388983707642aea662b63dfc2a87437295b62
willyam <lyamwp@gmail.com>
2022-05-21 22:32:23 -0300

    botoes do grid

--- Front-end/src/componentes/paginas/Sobre.js
+++ Front-end/src/componentes/paginas/Sobre.js
@@ -9,58 +9,63 @@ function Sobre() {
                         <h1>Sobre</h1>
                     </div>
                 </div>
-                </div>
-
-                <div className={styles.gridContainer}>
-                    <div className={styles.gridItem}><h2>Equipes</h2></div>
-                    <div className={styles.gridItem}><h2>Mídias</h2></div>
-                    <div className={styles.gridItem}><h2>Setores</h2></div>
-                    <div className={styles.gridItem}><h2> Atividades</h2></div>
-                    <div className={styles.gridItem}><h2>Parceiros</h2></div>
-                </div>
-                <div className={styles.gridContainer}>
-                    <div className={styles.gridItem}><button>equipe xxx</button>
-                        <button>equipe xxx</button>
-                        <button>equipe xxx</button>
-                        <button>equipe xxx</button>
-                        <button>equipe xxx</button>
-                        <button>equipe xxx</button>
-                        <button>equipe xxx</button></div>
-                    <div className={styles.gridItem}><button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button></div>
-                    <div className={styles.gridItem}><button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button></div>
-                    <div className={styles.gridItem}><button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button></div>
-                    <div className={styles.gridItem}><button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button>
-                            <button>equipe xxx</button></div>
-                </div>
-
-
-
-
             </div>
-            )
+
+            <div className={styles.gridContainer}>
+                <div className={styles.gridItem}><h2>Equipes</h2></div>
+                <div className={styles.gridItem}><h2>Mídias</h2></div>
+                <div className={styles.gridItem}><h2>Setores</h2></div>
+                <div className={styles.gridItem}><h2> Atividades</h2></div>
+                <div className={styles.gridItem}><h2>Parceiros</h2></div>
+            </div>
+            <div className={styles.gridContainer}>
+                <div className={styles.gridItem}>
+                    <button className={styles.btn}>Equipes</button>
+                    <button className={styles.btn}>Equipes</button>
+                    <button className={styles.btn}>Equipes</button>
+                    <button className={styles.btn}>Equipes</button>
+                    <button className={styles.btn}>Equipes</button>
+                    <button className={styles.btn}>Equipes</button>
+                    <button className={styles.btn}>Equipes</button></div>
+                <div className={styles.gridItem}>
+                    <button className={styles.btn}>Mídias</button>
+                    <button className={styles.btn}>Mídias</button>
+                    <button className={styles.btn}>Mídias</button>
+                    <button className={styles.btn}>Mídias</button>
+                    <button className={styles.btn}>Mídias</button>
+                    <button className={styles.btn}>Mídias</button>
+                    <button className={styles.btn}>Mídias</button></div>
+                <div className={styles.gridItem}>
+                    <button className={styles.btn}>Setores</button>
+                    <button className={styles.btn}>Setores</button>
+                    <button className={styles.btn}>Setores</button>
+                    <button className={styles.btn}>Setores</button>
+                    <button className={styles.btn}>Setores</button>
+                    <button className={styles.btn}>Setores</button>
+                    <button className={styles.btn}>Setores</button></div>
+                <div className={styles.gridItem}>
+                    <button className={styles.btn}>Atividades</button>
+                    <button className={styles.btn}>Atividades</button>
+                    <button className={styles.btn}>Atividades</button>
+                    <button className={styles.btn}>Atividades</button>
+                    <button className={styles.btn}>Atividades</button>
+                    <button className={styles.btn}>Atividades</button>
+                    <button className={styles.btn}>Atividades</button></div>
+                <div className={styles.gridItem}>
+                    <button className={styles.btn}>Parceiros</button>
+                    <button className={styles.btn}>Parceiros</button>
+                    <button className={styles.btn}>Parceiros</button>
+                    <button className={styles.btn}>Parceiros</button>
+                    <button className={styles.btn}>Parceiros</button>
+                    <button className={styles.btn}>Parceiros</button>
+                    <button className={styles.btn}>Parceiros</button></div>
+            </div>
+
+
+
+
+        </div>
+    )
 }
 
-            export default Sobre;
+export default Sobre;
